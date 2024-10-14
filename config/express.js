8a4987c6d4330c0d589ef165ad50b66d2992a9c7
masterdev777 <jacksonchen1103@gmail.com>
2024-10-14 16:13:11 +0800

    complete

--- config/express.js
+++ config/express.js
@@ -30,9 +30,9 @@ module.exports = () => {
     app.use(passport.initialize());
     // app.use(passport.session());
     app.use(express.static(path.join(__dirname, '../build')))
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.join(__dirname, '../build', 'index.html'));
-    //   });
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../build', 'index.html'));
+      });
 
     routers.map(router => {
         app.use(`/api/${router}`, require(`../routers/${router}.router`));
